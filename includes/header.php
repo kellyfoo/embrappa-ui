2841f7976f4d343fc2cdbac74406b331887bf48a
Kelly Foo <kelly@Kellys-MBP.T-mobile.com>
2020-07-15 15:11:35 -0400

    change title

--- includes/header.php
+++ includes/header.php
@@ -5,12 +5,12 @@
   <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:wght@400;700&display=swap" rel="stylesheet">
 
-  <title>TRANSPARENCY @ IBM</title>
+  <title>DIVERSPECTIVES @ IBM</title>
   <link rel="stylesheet" type="text/css" href="styles/theme.css" media="all" />
 </head>
 
 <div class="header">
   <header>
-    <h1> TRANSPARENCY @ IBM </h1>
+    <h1> DIVERSPECTIVES @ IBM </h1>
   </header>
 </div>
