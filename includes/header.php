7df61df917bc4ef5b763b992d6815d51c77ee7c0
kellyfoo <kellyfoo.01@gmail.com>
2020-07-15 13:54:30 -0400

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:wght@400;700&display=swap" rel="stylesheet">
+
+  <title>STAYWOKE @ IBM</title>
+  <link rel="stylesheet" type="text/css" href="styles/theme.css" media="all" />
+</head>
+
+<div class="header">
+  <header>
+    <h1> STAY WOKE @ IBM </h1>
+  </header>
+</div>
---
+++ login.php
@@ -0,0 +1,12 @@
+<?php include("includes/init.php"); ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include("includes/header.php"); ?>
+
+
+
+
+</html>
