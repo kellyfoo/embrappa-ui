43ea458353270d87f3222f2bcad06d9f936543c2
Depei Yu <depeiyu@ibm.com>
2020-07-15 16:17:42 -0400

    only upload

---
+++ upload.php
@@ -0,0 +1,32 @@
+<?php 
+include("includes/init.php"); 
+include("includes/header.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<div class=login>
+    <form method="post" action="home.php" enctype="multipart/form-data">
+          <div class="group_label_input">
+            <label class="form_title" for="email"> IBM Email: </label>
+            <input id="email" type="text" name="email"  />
+          </div>
+
+          <div class="group_label_input">
+            <label class="form_title" for="password"> Password: </label>
+            <input class = "password_input_text" id="password" type="text" name="password"  />
+          </div>
+
+          <div class="group_label_button">
+            <span></span>
+            <input name="submit_login" type="submit" value="LOG IN" />
+          </div>
+
+          <div class="group_label_input">
+            <a href="url">Forgot password</a>
+          </div>
+        </form>
+    
+      </div>
+
+</html>
